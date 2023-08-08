2002ee5316061b994cb261085e1630b3cbbc2659
ahoang2502 <athoang2502@gmail.com>
2023-08-08 11:52:57 +1000

    added login-functionality

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from "react";
+import { getProviders, signIn } from "next-auth/react";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | null;
+};
+type Providers = Record<string, Provider>;
 
 const AuthProvider = () => {
-  return (
-    <div>AuthProvider</div>
-  )
-}
+  const [providers, setProviders] = useState<Providers | null>(null);
 
-export default AuthProvider
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+
+      console.log(res)
+
+      setProviders(res);
+    };
+
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, index) => (
+          <button onClick={()=> signIn(provider?.id)} key={index}>{provider.id}</button>
+        ))}
+      </div>
+    );
+  }
+};
+
+export default AuthProvider;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { NavLinks } from "@/constants";
 import AuthProvider from "./AuthProvider";
+import { getCurrentUser } from "@/lib/session";
 
 import Link from "next/link";
+import Image from "next/image";
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
 
   return (
     <nav className="flexBetween navbar">
@@ -12,7 +14,7 @@ const Navbar = () => {
         <Link href="/">
           <h1 className="font-borel text-3xl">Drizzle</h1>
         </Link>
-        <ul className="md:flex hidden text-small gap-7">
+        <ul className="xl:flex hidden text-small gap-7">
           {NavLinks.map((link) => (
             <Link href={link.href} key={link.key}>
               {link.text}
@@ -22,9 +24,19 @@ const Navbar = () => {
       </div>
 
       <div className="flexCenter gap-4 ">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto <Link href="/create-project">Share work</Link>
+            {session?.user?.image && (
+              <Image
+                src={session.user.image}
+                width={40}
+                height={40}
+                className="rounded-full"
+                alt={session.user.name}
+              />
+            )}
+            
+            <Link href="/create-project">Share work</Link>
           </>
         ) : (
           <AuthProvider />
