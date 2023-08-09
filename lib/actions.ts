390f542f7c2489f51478a34c982dde15ccfabd76
ahoang2502 <athoang2502@gmail.com>
2023-08-09 14:54:56 +1000

    fixed typo

--- lib/actions.ts
+++ lib/actions.ts
@@ -21,7 +21,7 @@ const apiKey = isProduction
   ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
   : "letmein";
 const serverUrl = isProduction
-  ? process.env.NEXT_PUBlIC_SERVER_URL
+  ? process.env.NEXT_PUBLIC_SERVER_URL
   : "http://localhost:3000";
 
 const client = new GraphQLClient(apiUrl);
